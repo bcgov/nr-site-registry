4bc5f7ce4919188328a448062a242c323afa5713
Brendon O'Laney <Brendon.O'Laney@gov.bc.ca>
2024-10-07 15:53:50 -0700

    Use ternary operator rather than if clause

--- frontend/site-registry/src/app/features/details/SiteDetails.tsx
+++ frontend/site-registry/src/app/features/details/SiteDetails.tsx
@@ -261,56 +261,32 @@ const SiteDetails = () => {
     if (id) {
       dispatch(resetSaveSiteDetails(null));
       dispatch(setupSiteIdForSaving(id));
-      if (userType === UserType.External) {
-        Promise.all([
-          dispatch(fetchSnapshots(id ?? '')),
-          dispatch(getBannerType(id ?? '')),
-          dispatch(fetchMinistryContact('EMP')),
-          dispatch(fetchNotationClassCd()),
-          dispatch(fetchNotationTypeCd()),
-          dispatch(fetchNotationParticipantRoleCd()),
-          dispatch(fetchParticipantRoleCd()),
-          dispatch(
-            fetchSiteParticipants({ siteId: id ?? '', showPending: false }),
-          ),
-          dispatch(
-            fetchNotationParticipants({ siteId: id ?? '', showPending: false }),
-          ),
-          // should be based on condition for External and Internal User.
-          dispatch(fetchSitesDetails({ siteId: id ?? '', showPending: false })),
-          // dispatch(fetchNotationParticipants({ siteId: id ?? '', showPending: false})),
-        ])
-          .then(() => {
-            setIsLoading(false); // Set loading state to false after all API calls are resolved
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-          });
-      } else {
-        Promise.all([
-          dispatch(fetchSnapshots(id ?? '')),
-          dispatch(fetchMinistryContact('EMP')),
-          dispatch(fetchNotationClassCd()),
-          dispatch(fetchNotationTypeCd()),
-          dispatch(fetchNotationParticipantRoleCd()),
-          dispatch(fetchParticipantRoleCd()),
-          dispatch(
-            fetchSiteParticipants({ siteId: id ?? '', showPending: false }),
-          ),
-          dispatch(
-            fetchNotationParticipants({ siteId: id ?? '', showPending: false }),
-          ),
-          // should be based on condition for External and Internal User.
-          dispatch(fetchSitesDetails({ siteId: id ?? '', showPending: false })),
-          // dispatch(fetchNotationParticipants({ siteId: id ?? '', showPending: false})),
-        ])
-          .then(() => {
-            setIsLoading(false); // Set loading state to false after all API calls are resolved
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-          });
-      }
+      Promise.all([
+        dispatch(fetchSnapshots(id ?? '')),
+        userType === UserType.External
+          ? dispatch(getBannerType(id ?? ''))
+          : Promise.resolve(),
+        dispatch(fetchMinistryContact('EMP')),
+        dispatch(fetchNotationClassCd()),
+        dispatch(fetchNotationTypeCd()),
+        dispatch(fetchNotationParticipantRoleCd()),
+        dispatch(fetchParticipantRoleCd()),
+        dispatch(
+          fetchSiteParticipants({ siteId: id ?? '', showPending: false }),
+        ),
+        dispatch(
+          fetchNotationParticipants({ siteId: id ?? '', showPending: false }),
+        ),
+        // should be based on condition for External and Internal User.
+        dispatch(fetchSitesDetails({ siteId: id ?? '', showPending: false })),
+        // dispatch(fetchNotationParticipants({ siteId: id ?? '', showPending: false})),
+      ])
+        .then(() => {
+          setIsLoading(false); // Set loading state to false after all API calls are resolved
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
     }
   }, [id]);
 

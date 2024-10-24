121704351b2efcb219797a25df3c8d4855b7760b
Anton <anton.trublin+bcgov@gmail.com>
2024-10-24 11:56:43 -0700

    fix App test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,18 +3,26 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { useAuth } from 'react-oidc-context';
 import { store } from './app/Store';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 jest.mock('react-oidc-context', () => ({
   useAuth: jest.fn(),
 }));
 
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    path: '/',
+  },
+]);
+
 test('Renders Intro', () => {
   (useAuth as jest.Mock).mockReturnValue({ isAuthenticated: false });
   render(
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>,
   );
-  const formAccordion = screen.getByText(/Search Site Registry/i);
-  expect(formAccordion).toBeInTheDocument();
+  const siteName = screen.getByText(/SITE/i);
+  expect(siteName).toBeInTheDocument();
 });

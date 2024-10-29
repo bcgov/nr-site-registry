528037e00bcaf1cdb72b005fa6d7447a3234f44a
Adam Coard <adam.coard@aot-technologies.com>
2024-10-29 12:37:56 -0700

    Update initDb password setting

--- backend/initDB.sh
+++ backend/initDB.sh
@@ -50,9 +50,14 @@ echo "migrations completed"
 # In OpenShift, $SEED_DATA_PATH should point to a PVC that contains storage
 # See ora2pg/openshift/readme.md in this repo for setup of that.
 if [ -n "$SEED_DATA_PATH" ]; then
+    # TODO TODO: DO NOT ACCEPT WITHOUT
+    # Need a safety to only run seed file if db is empty or something? Experiment with restarting it, try adding records, etc.
+    # TODO:  Move this to separate initContainer, just left here to verify it works as we're close.
+
     # Run the seed data SQL file
     echo "Seed data set, attempting to load."
-    psql -h "$POSTGRESQL_HOST" -d "$POSTGRES_DATABASE" -U "$POSTGRES_ADMIN_USERNAME" -f "$SEED_DATA_PATH"
+    PGPASSWORD="$POSTGRES_ADMIN_PASSWORD" psql -h "$POSTGRESQL_HOST" -d "$POSTGRES_DATABASE" -U "$POSTGRES_ADMIN_USERNAME" -f "$SEED_DATA_PATH"
+    # psql: error: connection to server at "nr-site-registry-143-bitnami-pg" (10.98.70.107), port 5432 failed: fe_sendauth: no password supplied
     echo "Seed data successfully loaded."
 else
     echo "SEED_DATA_PATH is not set. Skipping seed data loading."

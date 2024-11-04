093c65c10a3d5d49d975e5380cc6f5c79c72df4f
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2024-11-04 09:51:41 -0800

    fix: remove initDB.sh from Dockerfile.local as the extensions are created as part of typeorm migrations

--- backend/local_start.sh
+++ backend/local_start.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-# create DB dependencies - database, schema
-sh initDB.sh
 
+#Make sure you change line endings to LF
+
+if [ ! "$POSTGRESQL_HOST" ];
+then
+   echo 'Sourcing from .env'
+   . ./.env
+else
+    echo 'Environment variables set...'
+fi
+
+
+# create schema
+psql "user=$POSTGRES_ADMIN_USERNAME password=$POSTGRES_ADMIN_PASSWORD host=$POSTGRESQL_HOST port=$POSTGRESQL_PORT dbname=$POSTGRES_DATABASE" -c "CREATE SCHEMA IF NOT EXISTS $POSTGRES_DB_SCHEMA AUTHORIZATION \"$POSTGRES_DB_USERNAME\""
+
+echo "schema created"
 # run type orm migrations
 npm run typeorm:run-migrations
 

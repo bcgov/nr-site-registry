d436042ae98320ee3b9fd748401401f142a78fed
nupurdixit13 <nupur.dixit@gov.bc.ca>
2024-12-12 11:42:15 -0800

    Addressed few more comments

--- frontend/src/app/features/details/summary/SummaryInfo.tsx
+++ frontend/src/app/features/details/summary/SummaryInfo.tsx
@@ -2,25 +2,13 @@ import React, { useContext, useEffect, useRef } from 'react';
 import PanelWithUpDown from '../../../components/simple/PanelWithUpDown';
 // const Map: any = require('../../../../../node_modules/react-parcelmap-bc/dist/Map').default;
 // @ts-ignore
-import { Map } from 'leaflet';
 import SummaryForm from '../SummaryForm';
 import { ApproveRejectButtons } from '../../../components/approve/ApproveReject';
 import { MapContainer, useMap } from 'react-leaflet';
-import { SiteMarkers } from '../../map/siteMarkers/SiteMarkers';
-import { LatLngBounds, LatLngTuple } from 'leaflet';
 import { TileLayer } from 'react-leaflet';
 import { SiteMarker } from '../../map/siteMarkers/SiteMarker';
 import './Summary.css';
 import 'leaflet/dist/leaflet.css';
-import { useMediaQuery, useTheme } from '@mui/material';
-import { MAP_FLY_OPTIONS, getZoom } from '../../map/mapOptions';
-import { get } from 'http';
-import { MapSearchQueryParamsContext } from '../../map/mapSearchQueryParamsContext/MapSearchQueryParamsContext';
-import {
-  useMapSearchQuery,
-  useMapSearch_FindSiteBySiteIdQuery,
-} from '../../../../graphql/generated';
-import { Site } from '../../map/MapView';
 import { set } from 'date-fns';
 
 export interface ISummaryInfo {
@@ -48,37 +36,6 @@ const SummaryInfo: React.FC<ISummaryInfo> = ({
       console.log('Approve Handler Not Provided');
     });
 
-  //const CENTER_OF_BC: LatLngTuple = [53.7267, -127.6476];
-
-  const { data } = useMapSearch_FindSiteBySiteIdQuery({
-    variables: { siteId: siteData.id },
-  });
-
-  const selectedSiteData = data?.findSiteBySiteId.data;
-
-  // const mapRef = useRef<Map>(null);
-  // const map_fly_options = { animate: false, duration: 0.2 };
-
-  // const MapWithMarker = ({ site }: { site: any }) => {
-  //   const map = useMap();
-
-  //   useEffect(() => {
-  //     if (!site.latdeg || !site.longdeg) return;
-  //     map.flyTo(
-  //       {
-  //         lat: site.latdeg,
-  //         lng: site.longdeg,
-  //       },
-  //       getZoom(map),
-  //       map_fly_options,
-  //     );
-  //   }, [site, map]);
-
-  //   return (
-  //     site && <SiteMarker position={{ lat: site.latdeg, lng: site.longdeg }} />
-  //   );
-  // };
-
   return (
     <PanelWithUpDown
       label="Location Details"
@@ -87,12 +44,11 @@ const SummaryInfo: React.FC<ISummaryInfo> = ({
           <div className="col-12 col-lg-6">
             <MapContainer
               center={{
-                lat: selectedSiteData?.latdeg ?? 0,
-                lng: selectedSiteData?.longdeg ?? 0,
+                lat: siteData.latdeg,
+                lng: siteData.longdeg,
               }}
               zoom={6}
               zoomControl={false}
-              //ref={mapRef}
               className="map-container"
             >
               <TileLayer
@@ -101,8 +57,8 @@ const SummaryInfo: React.FC<ISummaryInfo> = ({
               />
               <SiteMarker
                 position={{
-                  lat: selectedSiteData?.latdeg ?? 0,
-                  lng: selectedSiteData?.longdeg ?? 0,
+                  lat: siteData.latdeg,
+                  lng: siteData.longdeg,
                 }}
               />
             </MapContainer>

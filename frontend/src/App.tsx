3b3caa4158564fa79178113ee472cb5f84d58bc6
Anton <anton.trublin+bcgov@gmail.com>
2025-01-22 15:26:18 -0800

    SRS-766: refresh access token if expired

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useAuth } from 'react-oidc-context';
 import './App.css';
 import Header from './app/components/navigation/Header';
 import { Outlet } from 'react-router-dom';
@@ -8,6 +10,19 @@ import SideBar from './app/components/navigation/SideBar';
 import '@bcgov/bc-sans/css/BCSans.css';
 
 function App() {
+  const { isAuthenticated, signinSilent, events, user } = useAuth();
+
+  useEffect(() => {
+    if (user?.expired) {
+      // Access token expired, trying to refresh
+      signinSilent();
+    }
+    // the `return` is important - addAccessTokenExpiring() returns a cleanup function
+    return events.addAccessTokenExpiring(() => {
+      signinSilent();
+    });
+  }, [events, signinSilent, isAuthenticated, user]);
+
   return (
     <div className="container-fluid p-0">
       <Header />

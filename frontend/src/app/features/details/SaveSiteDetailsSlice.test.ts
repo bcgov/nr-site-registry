224843e10050fc0eb4180d2ca2f5aa52a6a68784
Brendon O'Laney <Brendon.O'Laney@gov.bc.ca>
2025-01-27 15:35:31 -0800

    Correct describe level for spec.

--- frontend/src/app/features/details/SaveSiteDetailsSlice.test.ts
+++ frontend/src/app/features/details/SaveSiteDetailsSlice.test.ts
@@ -2,68 +2,70 @@ import { RequestStatus } from '../../helpers/requests/status';
 import reducer, { resetSaveSiteDetails } from './SaveSiteDetailsSlice';
 
 describe('siteDetailsSlice', () => {
-  it('nullifies data', () => {
-    const initialState = {
-      saveRequestStatus: RequestStatus.loading,
-      notationData: ['notationData'],
-      siteParticipantData: ['siteParticipantData'],
-      documentsData: ['documentsData'],
-      landHistoriesData: ['landHistoriesData'],
-      parcelDescriptionsData: ['parcelDescriptionsData'],
-      profilesData: ['profilesData'],
-      siteAssociationsData: ['siteAssociationsData'],
-      siteId: '123',
-      sitesSummary: ['sitesSummary'],
-    };
+  describe('resetSaveSiteDetails', () => {
+    it('nullifies data', () => {
+      const initialState = {
+        saveRequestStatus: RequestStatus.loading,
+        notationData: ['notationData'],
+        siteParticipantData: ['siteParticipantData'],
+        documentsData: ['documentsData'],
+        landHistoriesData: ['landHistoriesData'],
+        parcelDescriptionsData: ['parcelDescriptionsData'],
+        profilesData: ['profilesData'],
+        siteAssociationsData: ['siteAssociationsData'],
+        siteId: '123',
+        sitesSummary: ['sitesSummary'],
+      };
 
-    const result = reducer(initialState, resetSaveSiteDetails(null));
-    expect(result).toEqual(
-      expect.objectContaining({
-        notationData: null,
-        siteParticipantData: null,
-        documentsData: null,
-        landHistoriesData: null,
-        parcelDescriptionsData: null,
-        profilesData: null,
-        siteAssociationsData: null,
-        sitesSummary: null,
-      }),
-    );
-  });
+      const result = reducer(initialState, resetSaveSiteDetails(null));
+      expect(result).toEqual(
+        expect.objectContaining({
+          notationData: null,
+          siteParticipantData: null,
+          documentsData: null,
+          landHistoriesData: null,
+          parcelDescriptionsData: null,
+          profilesData: null,
+          siteAssociationsData: null,
+          sitesSummary: null,
+        }),
+      );
+    });
 
-  it('sets saveRequestStatus to idle', () => {
-    const initialState = {
-      saveRequestStatus: RequestStatus.loading,
-      notationData: ['notationData'],
-      siteParticipantData: ['siteParticipantData'],
-      documentsData: ['documentsData'],
-      landHistoriesData: ['landHistoriesData'],
-      parcelDescriptionsData: ['parcelDescriptionsData'],
-      profilesData: ['profilesData'],
-      siteAssociationsData: ['siteAssociationsData'],
-      siteId: '123',
-      sitesSummary: ['sitesSummary'],
-    };
+    it('sets saveRequestStatus to idle', () => {
+      const initialState = {
+        saveRequestStatus: RequestStatus.loading,
+        notationData: ['notationData'],
+        siteParticipantData: ['siteParticipantData'],
+        documentsData: ['documentsData'],
+        landHistoriesData: ['landHistoriesData'],
+        parcelDescriptionsData: ['parcelDescriptionsData'],
+        profilesData: ['profilesData'],
+        siteAssociationsData: ['siteAssociationsData'],
+        siteId: '123',
+        sitesSummary: ['sitesSummary'],
+      };
 
-    const result = reducer(initialState, resetSaveSiteDetails(null));
-    expect(result.saveRequestStatus).toEqual(RequestStatus.idle);
-  });
+      const result = reducer(initialState, resetSaveSiteDetails(null));
+      expect(result.saveRequestStatus).toEqual(RequestStatus.idle);
+    });
 
-  it('maintains siteId', () => {
-    const initialState = {
-      saveRequestStatus: RequestStatus.loading,
-      notationData: ['notationData'],
-      siteParticipantData: ['siteParticipantData'],
-      documentsData: ['documentsData'],
-      landHistoriesData: ['landHistoriesData'],
-      parcelDescriptionsData: ['parcelDescriptionsData'],
-      profilesData: ['profilesData'],
-      siteAssociationsData: ['siteAssociationsData'],
-      siteId: '123',
-      sitesSummary: ['sitesSummary'],
-    };
+    it('maintains siteId', () => {
+      const initialState = {
+        saveRequestStatus: RequestStatus.loading,
+        notationData: ['notationData'],
+        siteParticipantData: ['siteParticipantData'],
+        documentsData: ['documentsData'],
+        landHistoriesData: ['landHistoriesData'],
+        parcelDescriptionsData: ['parcelDescriptionsData'],
+        profilesData: ['profilesData'],
+        siteAssociationsData: ['siteAssociationsData'],
+        siteId: '123',
+        sitesSummary: ['sitesSummary'],
+      };
 
-    const result = reducer(initialState, resetSaveSiteDetails(null));
-    expect(result.siteId).toEqual('123');
+      const result = reducer(initialState, resetSaveSiteDetails(null));
+      expect(result.siteId).toEqual('123');
+    });
   });
 });

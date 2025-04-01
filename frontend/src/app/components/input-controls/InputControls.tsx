e976d88266265e528083eb8a5655d61310ce0295
midhun-aot <midhun.murali@aot-technologies.com>
2025-04-01 07:19:19 -0700

    Fixing Table Column Border Visibility

--- frontend/src/app/components/input-controls/InputControls.tsx
+++ frontend/src/app/components/input-controls/InputControls.tsx
@@ -37,7 +37,7 @@ const renderTableCell = (
 ) => {
   return (
     <td
-      className={`"table-border-light align-content-center" ${stickyCol ? 'positionSticky' : ''}`}
+      className={`table-border-light align-content-center ${stickyCol ? 'positionSticky' : ''}`}
     >
       {content}
     </td>

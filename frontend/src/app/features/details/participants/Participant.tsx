a0a5e4823e31130367d4b726ffeda35941263831
Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>
2025-05-06 12:51:57 -0700

    handle meta data null issue.

--- frontend/src/app/features/details/participants/Participant.tsx
+++ frontend/src/app/features/details/participants/Participant.tsx
@@ -126,8 +126,9 @@ const Participants: React.FC<IComponentProps> = ({ showPending = false }) => {
         });
 
         // Store result in cache if successful
-        if (response?.data?.data?.getPeopleOrgsCd?.success) {
-          resultCache[searchParam] = response.data.data.getPeopleOrgsCd.data;
+        const { data, success } = response?.data?.data?.getPeopleOrgsCd;
+        if (success && data?.length > 0) {
+          resultCache[searchParam] = data;
           return response.data.data.getPeopleOrgsCd;
         }
       } catch (error) {

7a104154db7aede6d22e84bc087dbe75620f2866
midhun-aot <midhun.murali@aot-technologies.com>
2025-05-26 10:14:19 -0700

    Fixed frontend test case

--- frontend/src/app/features/site/Search.test.tsx
+++ frontend/src/app/features/site/Search.test.tsx
@@ -83,7 +83,7 @@ describe('Search Component', () => {
     renderWithProviders(<Search />, store);
 
     const searchInput = screen.getByPlaceholderText(
-      'Search for site address or name',
+      'Search for site address or name or pid',
     );
     fireEvent.change(searchInput, { target: { value: 'test' } });
 
@@ -102,7 +102,7 @@ describe('Search Component', () => {
     renderWithProviders(<Search />, store);
 
     const searchInput = screen.getByPlaceholderText(
-      'Search for site address or name',
+      'Search for site address or name or pid',
     );
     fireEvent.change(searchInput, { target: { value: 'test' } });
 

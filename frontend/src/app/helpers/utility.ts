d803ea39249aeb3c57a690ca3548a732f388e8e7
midhun-aot <105463561+midhun-aot@users.noreply.github.com>
2025-06-03 09:19:06 -0700

    Uncommented code to refer env value for site-registrar role (#311)
    Co-authored-by: jaspal-aot <163812444+jaspalsingh-aot@users.noreply.github.com>

--- frontend/src/app/helpers/utility.ts
+++ frontend/src/app/helpers/utility.ts
@@ -225,8 +225,9 @@ export const isUserOfType = (roleType: UserRoleType) => {
         }
       case 'sr':
         const srUserRole =
-          //  process.env?.REACT_APP_SITE_REGISTRAR_USER_ROLE
-          // ?? ((window as any)?._env_?.REACT_APP_SITE_REGISTRAR_USER_ROLE) ??
+          process.env.REACT_APP_SITE_REGISTRAR_USER_ROLE ||
+          ((window as any)._env_ &&
+            (window as any)._env_.REACT_APP_SITE_REGISTRAR_USER_ROLE) ||
           'site-site-registrar';
 
         if (userRoles.includes(srUserRole)) {

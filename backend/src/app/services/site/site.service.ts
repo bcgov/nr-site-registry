a2a3637df674ad6014221c4fc92b5d082cdecb9e
midhun-aot <105463561+midhun-aot@users.noreply.github.com>
2025-07-03 10:15:17 -0700

    fix: Replace entity with DTO (#327)
    * changes for SRS-927 & SRS-921
    
    * changes for securing the controller
    
    * adding logger to csv function
    
    * replacing with DTO
    
    * replacing with DTO
    
    * Updated Class Name
    
    * Modified DTO to reuse and remove duplicate
    
    * Updated class name
    
    * Updated class name

--- backend/src/app/services/site/site.service.ts
+++ backend/src/app/services/site/site.service.ts
@@ -8,6 +8,7 @@ import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { Brackets, EntityManager, In, Repository } from 'typeorm';
 import {
   FetchSiteDetail,
+  FetchSiteDetailsResponse,
   FetchSiteResponse,
   SearchSiteResponse,
 } from '../../dto/response/genericResponse';
@@ -559,7 +560,7 @@ export class SiteService {
   async findSiteBySiteId(siteId: string, pending: boolean, userInfo: any) {
     this.sitesLogger.log('SiteService.findSiteBySiteId() start');
     this.sitesLogger.debug('SiteService.findSiteBySiteId() start');
-    const response = new FetchSiteDetail();
+    const response = new FetchSiteDetailsResponse();
 
     response.httpStatusCode = 200;
 

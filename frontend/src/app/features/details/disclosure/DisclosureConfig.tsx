cfa3aed4bdf045a8c20809525ac428faa9c0817f
Jaspal.Singh-AOT <163812444+jaspalsingh-aot@users.noreply.github.com>
2025-10-24 10:59:52 -0700

    feat: SDS Schedule 2 Updated Content Dropdown

--- frontend/src/app/features/details/disclosure/DisclosureConfig.tsx
+++ frontend/src/app/features/details/disclosure/DisclosureConfig.tsx
@@ -5,9 +5,11 @@ import {
 } from '../../../components/input-controls/IFormField';
 import { ColumnSize, TableColumn } from '../../../components/table/TableColumn';
 import { SRVisibility } from '../../../helpers/requests/srVisibility';
+import { SiteDetailsMode } from '../dto/SiteDetailsMode';
 
 export const siteDisclosureConfig = (
-  schedule2Refs: { key: string; value: string }[],
+  schedule2Refs: { key: string; value: string; metaData: string }[],
+  userMode?: SiteDetailsMode,
 ) => {
   const disclosureStatementConfig: IFormField[][] = [
     [
@@ -211,7 +213,7 @@ export const siteDisclosureConfig = (
     ],
   ];
 
-  const disclosureScheduleInternalConfig: TableColumn[] = [
+  const disclosureScheduleConfig: TableColumn[] = [
     {
       id: 1,
       displayName: 'Schedule 2 Reference',
@@ -222,31 +224,20 @@ export const siteDisclosureConfig = (
         label: '',
         placeholder: 'Select Schedule 2 reference.',
         graphQLPropertyName: 'schedule2ReferenceCode',
-        options: schedule2Refs,
+        options: schedule2Refs?.map((ref) => ({
+          key: ref.key,
+          value:
+            userMode === SiteDetailsMode.EditMode ? ref.metaData : ref.value,
+        })),
         value: '',
         colSize: 'col-lg-6 col-md-6 col-sm-12',
         customInputTextCss: 'custom-disclosure-input-text-tbl',
         tableMode: true,
       },
-      columnSize: ColumnSize.Default,
-    },
-    {
-      id: 2,
-      displayName: 'Description',
-      active: true,
-      graphQLPropertyName: 'description',
-      displayType: {
-        type: FormFieldType.Text,
-        label: 'Description',
-        isDisabled: true,
-        placeholder: 'Select Schedule 2 reference.',
-        graphQLPropertyName: 'description',
-        value: '',
-        colSize: 'col-lg-6 col-md-6 col-sm-12',
-        customInputTextCss: 'custom-disclosure-input-text-tbl',
-        tableMode: true,
-      },
-      columnSize: ColumnSize.Triple,
+      columnSize:
+        userMode === SiteDetailsMode.EditMode
+          ? ColumnSize.Default
+          : ColumnSize.XtraSmall,
     },
     {
       id: 3,
@@ -268,6 +259,27 @@ export const siteDisclosureConfig = (
     },
   ];
 
+  const disclosureScheduleDescription: TableColumn[] = [
+    {
+      id: 2,
+      displayName: 'Description',
+      active: true,
+      graphQLPropertyName: 'description',
+      displayType: {
+        type: FormFieldType.Text,
+        label: 'Description',
+        isDisabled: true,
+        placeholder: 'Select Schedule 2 reference.',
+        graphQLPropertyName: 'description',
+        value: '',
+        colSize: 'col-lg-6 col-md-6 col-sm-12',
+        customInputTextCss: 'custom-disclosure-input-text-tbl',
+        tableMode: true,
+      },
+      columnSize: ColumnSize.Triple,
+    },
+  ];
+
   const disclosureScheduleExternalConfig: TableColumn[] = [
     {
       id: 1,
@@ -285,7 +297,7 @@ export const siteDisclosureConfig = (
         customInputTextCss: 'custom-disclosure-input-text-tbl',
         tableMode: true,
       },
-      columnSize: ColumnSize.Default,
+      columnSize: ColumnSize.XtraSmall,
     },
     {
       id: 2,
@@ -368,6 +380,15 @@ export const siteDisclosureConfig = (
       value: SRVisibility.HideSR,
     },
   ];
+
+  const disclosureScheduleInternalConfig =
+    userMode !== SiteDetailsMode.EditMode
+      ? [
+          ...disclosureScheduleConfig.slice(0, 1),
+          ...disclosureScheduleDescription,
+          ...disclosureScheduleConfig.slice(1),
+        ]
+      : [...disclosureScheduleConfig];
 
   return {
     disclosureStatementConfig,

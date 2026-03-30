e09dc8f2336e88fa041a48d40127a74bde0b1be4
Urmi Kataria <urmi.kataria@gov.bc.ca>
2026-03-30 13:57:00 -0700

    fix

--- frontend/src/app/features/details/landUses/LandUses.test.tsx
+++ frontend/src/app/features/details/landUses/LandUses.test.tsx
@@ -4,6 +4,38 @@ import LandUses from './LandUses';
 import { UserRoleType } from '../../../helpers/utility';
 
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({ id: '123' }),
+}));
+
+/* =========================
+   MOCK react-redux
+========================= */
+jest.mock('react-redux', () => ({
+  useDispatch: () => jest.fn(),
+  useSelector: () => ({
+    landUses: [],
+    landUseCodes: [],
+    landUsesFetchRequestStatus: 'idle',
+    landUseCodesFetchRequestStatus: 'idle',
+  }),
+}));
+
+/* =========================
+   MOCK utilities
+========================= */
+jest.mock('../../../helpers/utility', () => ({
+  getUser: jest.fn(() => ({ id: 'test-user' })),
+  isUserOfType: jest.fn(),
+  UserRoleType: {
+    INTERNAL: 'INTERNAL',
+    CLIENT: 'CLIENT',
+    PUBLIC: 'PUBLIC',
+  },
+}));
+
+
 /* =========================
    TESTS
 ========================= */

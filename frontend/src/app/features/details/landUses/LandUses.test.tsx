2d504868409ce2261a1ad55c1a02cffab1c38d70
Urmi Kataria <urmi.kataria@gov.bc.ca>
2026-03-30 13:34:44 -0700

    test case timeout fix

--- frontend/src/app/features/details/landUses/LandUses.test.tsx
+++ frontend/src/app/features/details/landUses/LandUses.test.tsx
@@ -3,6 +3,12 @@ import { render, screen } from '@testing-library/react';
 import LandUses from './LandUses';
 import { UserRoleType } from '../../../helpers/utility';
 
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: () => ({ id: '123' }),
+}));
+
 /* =========================
    MOCK react-redux
 ========================= */

b9d5d4d39f64d744726db4aac3d50b8aa7cd844d
midhun-aot <midhun.murali@aot-technologies.com>
2026-05-20 08:14:01 -0700

    removing commented code

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,8 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-// Polyfill TextEncoder/TextDecoder for jsdom (required by @react-pdf/renderer)
-// const { TextEncoder, TextDecoder } = require('util');
-// global.TextEncoder = TextEncoder;
-// global.TextDecoder = TextDecoder;
